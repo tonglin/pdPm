9526e3e87ed91aadc7574f506704160def3b07da
tonglin <chhun.tonglin@gmail.com>
2013-12-13 22:58:05 +0700

    config database

--- public_html/typo3conf/AdditionalConfiguration.php
+++ public_html/typo3conf/AdditionalConfiguration.php
@@ -2,7 +2,11 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
-
+## Database connection
+$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 'pdPm'; // Your database name, e.g: local-luvwise
+$GLOBALS['TYPO3_CONF_VARS']['DB']['host']     = 'localhost'; // Your database host, e.g: localhost
+$GLOBALS['TYPO3_CONF_VARS']['DB']['password'] = 'root'; // Your database password, e.g: ess:3
+$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = 'root'; // Your database username, e.g: root
 // You may add PHP code here, wich is executed on every request after the configuration is loaded.
 // The code here should only manipulate TYPO3_CONF_VARS for example to set the database configuration
 // dependent to the requested environment.
